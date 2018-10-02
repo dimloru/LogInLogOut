7a79bcca3789037b2b6f709c7ac7c9d18a40f44b
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-10-02 17:21:59 +0300

    some refactoring

--- src/main/java/com/dimlo/model/Role.java
+++ src/main/java/com/dimlo/model/Role.java
@@ -1,5 +1,5 @@
 package com.dimlo.model;
 
 public enum Role {
-    USER;
+    USER
 }
--- src/main/java/com/dimlo/repository/UserRepository.java
+++ src/main/java/com/dimlo/repository/UserRepository.java
@@ -3,11 +3,6 @@ package com.dimlo.repository;
 import com.dimlo.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-//import User;
-
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
-
 public interface UserRepository extends CrudRepository<User, Integer> {
     User findByEmail(String email);
 
