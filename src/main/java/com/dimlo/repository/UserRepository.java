e1563203bc439b82ce7fcc4626f7dd1668e0a95c
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-10-02 18:40:05 +0300

    readme, UTF in properties

--- src/main/java/com/dimlo/repository/UserRepository.java
+++ src/main/java/com/dimlo/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.dimlo.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+
     User findByEmail(String email);
 
 }
