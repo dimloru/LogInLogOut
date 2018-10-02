d4c02ebaf2da28f3e5b2f1e1749f7f58a4472168
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-10-02 10:37:32 +0300

    Pre bd-backed login Have wrong email format issue, sending email w/pass

--- src/main/java/com/dimlo/MainController.java
+++ src/main/java/com/dimlo/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(path="/db")
+@RequestMapping(path="/db") //get rid of it
 public class MainController {
     @Autowired
     private UserRepository userRepository;
@@ -17,20 +17,19 @@ public class MainController {
     public String addNewUserPost (@RequestParam String email) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
-                
-        User existing = userRepository.findByEmail(email);
 
-        if (existing == null) {
+        if (email != null && !email.isEmpty() &&
+                userRepository.findByEmail(email) == null ) {
             User newUser = new User();
             newUser.setEmail(email);
             newUser.setPassword(PasswordGenerator.generate());
 
-            userRepository.save(newUser);
+            userRepository.save(newUser); //wrong format exception
         } else {
             System.out.println("NOT NEW NOT NEW NOT NEW NOT NEW NOT NEW");
+            return "redirect:/login"; // redirectAttributes
             // do something
         }
-
 
 
         return "redirect:login"; // different view like "an email with a pass has been sent..."
@@ -42,26 +41,4 @@ public class MainController {
         // This returns a JSON or XML with the users
         return userRepository.findAll();
     }
-//
-//    @GetMapping(path="/home")
-//    public String home() {
-//        return "home";
-//    }
-//
-//    @GetMapping(path="/hello")
-//    public String hello() {
-//        return "hello";
-//    }
-//
-//    @GetMapping(path="/login")
-//    public String login() {
-//        return "login";
-//    }
-
-
-
-//    registry.addViewController("/home").setViewName("home");
-//        registry.addViewController("/").setViewName("home");
-//        registry.addViewController("/hello").setViewName("hello");
-//        registry.addViewController("/login").setViewName("login");
 }
