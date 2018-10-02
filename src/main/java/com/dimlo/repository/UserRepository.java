3c77c4c85dbdf32b075e3ae9ec5ef5e230601d3c
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-10-02 10:15:22 +0300

    Done email duplication issue Sign in and Register on Main page

--- src/main/java/com/dimlo/repository/UserRepository.java
+++ src/main/java/com/dimlo/repository/UserRepository.java
@@ -9,5 +9,6 @@ import org.springframework.data.repository.CrudRepository;
 // CRUD refers Create, Read, Update, Delete
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+    User findByEmail(String email);
 
 }
